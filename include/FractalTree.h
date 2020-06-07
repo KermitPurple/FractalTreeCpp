2b4959354a3e63d9e891a9df722692a0c6774520
Shane McDonough <shanemcdman@gmail.com>
2020-06-07 00:49:50 -0700

    Add constructors to FractalTree

--- include/FractalTree.h
+++ include/FractalTree.h
@@ -1,7 +1,9 @@
 class FractalTree{
 private:
-    float offset_angle = 45;
 public:
+    float offset_angle = 45;
+    FractalTree();
+    FractalTree(float angle);
     void set_offset_angle(float angle);
     void add_to_offset_angle(float angle);
     void draw();
--- src/FractalTree.cpp
+++ src/FractalTree.cpp
@@ -1,6 +1,14 @@
 #include<GL/glut.h>
 #include<FractalTree.h>
 
+FractalTree::FractalTree(){
+    offset_angle = 45;
+}
+
+FractalTree::FractalTree(float angle){
+    offset_angle = angle;
+}
+
 void FractalTree::set_offset_angle(float angle){
     offset_angle = angle;
 }
